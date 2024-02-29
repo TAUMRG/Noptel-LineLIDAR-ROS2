3b109931c1bf259461085a454c248548dde234be
Eetu Friman (TAU) <eetu.friman@tuni.fi>
2024-02-29 16:39:53 +0200

    Revert "Update default.py"
    This reverts commit e5d7cd88f743fd011ba575f8321f3bfd9a7a1cb8.

--- linelidarclass/default.py
+++ linelidarclass/default.py
@@ -19,3 +19,38 @@ Tested on:
 Authors:
   PCo
 """
+
+from __future__ import annotations
+
+
+
+## Default parameters
+# @defgroup DefaultParameters Default parameters
+
+## Default UDP port
+_ll_default_port: int
+_ll_default_port = 9907
+
+## Default UDP communication timeout in seconds
+_ll_default_udp_timeout: float
+_ll_default_udp_timeout = 1.5 #s
+
+## Default SSH communication timeout in seconds
+_ll_default_ssh_timeout: float
+_ll_default_ssh_timeout = 5 #s
+
+## Default reboot timeout in seconds
+_ll_default_reboot_timeout: float
+_ll_default_reboot_timeout = 5 #s
+
+## Default number of retries for fault-tolerance
+_ll_default_retries: int
+_ll_default_retries = 1
+
+## Default depth of the sent commands log
+_ll_default_sent_cmds_log_depth: int
+_ll_default_sent_cmds_log_depth = 16
+
+## Default path for the Python executable on the SSH relay host
+_ll_default_ssh_python_path: str
+_ll_default_ssh_python_path = "python"
