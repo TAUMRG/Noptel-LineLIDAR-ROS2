760d77b90572eab5f0f99509eb5679b0f6036169
Eetu-Friman <70322230+Eetu-Friman@users.noreply.github.com>
2024-04-03 12:49:01 +0000

    Add files via upload

---
+++ launch/ll.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+   return LaunchDescription([
+      Node(
+         package='Noptel_LineLIDAR_ROS2',
+         executable='ll_ros2',
+         name='linelidar',
+         parameters=[
+         	{'addr': '192.168.10.98'},
+         	{'freq': 20},
+         ]
+      ),
+   ])
